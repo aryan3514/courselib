9398b12a203c3a0bd00673628a52f0904d88a411
Akhilesh Niol <akhilesh@Akhileshs-MacBook-Pro.local>
2022-02-08 21:05:29 +0530

    login-logout authentication UI in html

--- app.py
+++ app.py
@@ -1,19 +1,91 @@
-from flask import Flask
+from flask import (Flask, g, flash, redirect, render_template, request, session, url_for)
 import db
 
 
-cur = db.start_db()
-if cur!=None:
-	q= "begin;"+open("create_table.sql", "r").read()+"commit;"
-	cur.execute(q) 
-	print("table created\n")
-
-
 app = Flask(__name__)
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 
+@app.route('/studentpage')
+def studentpage():
+    load_logged_in_student()
+    return render_template('student_page.html')
+
+@app.route('/adminpage')
+def adminpage():
+    load_logged_in_admin()
+    return render_template('admin_page.html')
+
+@app.route('/adminlogin', methods=('GET', 'POST'))
+def adminlogin():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        conn = db.start_db()
+        cur = conn.cursor()
+        error = None
+        cur.execute("SELECT password FROM admin_info where username = %s", (username,))
+        user = cur.fetchone()
+        print(user)
+        if user is None:
+            error = 'Incorrect username.'
+        elif (user[0] != password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = username
+            return redirect(url_for('adminpage'))
+        flash(error)
+    return render_template('admin_login.html')
+
+@app.route('/studentlogin', methods=('GET', 'POST'))
+def studentlogin():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        conn = db.start_db()
+        cur = conn.cursor()
+        error = None
+        cur.execute("SELECT password FROM student_login_info where username = %s", (username,))
+        user = cur.fetchone()
+        print(user)
+        if user is None:
+            error = 'Incorrect username.'
+        elif (user[0] != password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = username
+            return redirect(url_for('studentpage'))
+        flash(error)
+    return render_template('student_login.html')
+
+def load_logged_in_admin():
+    username = session.get('user_id')
+    if username is None:
+        g.user = None
+    else:
+        conn = db.start_db()
+        cur = conn.cursor()
+        cur.execute('SELECT username FROM admin_info WHERE username = %s', (username,))
+        g.user = cur.fetchone()
+
+def load_logged_in_student():
+    username = session.get('user_id')
+    if username is None:
+        g.user = None
+    else:
+        conn = db.start_db()
+        cur = conn.cursor()
+        cur.execute('SELECT username FROM student_login_info WHERE username = %s', (username,))
+        g.user = cur.fetchone()
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('adminlogin'))
 
 
-@app.route('/')
-def index():
-    return 'Index Page!!!'
