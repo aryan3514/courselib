45d80264bbc6e94717fe9aa03b49f3644191b88d
Akhilesh Niol <akhilesh@Akhileshs-MacBook-Pro.local>
2022-02-10 02:23:44 +0530

    solved all the issues, i.e. 1. ordering, 2. type casting issues where empty strings were present instead of null

--- create_table.sql
+++ create_table.sql
@@ -8,9 +8,9 @@ CREATE table IF NOT EXISTS courses(
 );
 
 CREATE table IF NOT EXISTS assessments(
-    id_assessment int NOT NULL,
     code_module text,
     code_presentation text,
+    id_assessment int NOT NULL,
     assessment_type text,
     date int,
     weight float,
@@ -19,18 +19,18 @@ CREATE table IF NOT EXISTS assessments(
 );
 
 CREATE table IF NOT EXISTS studentInfo(
-    id_student bigint NOT NULL,
-    gender VARCHAR (3),
     code_module text,
     code_presentation text,
-    imd_brand text,
-    highest_education text,
-    age_band text,
+    id_student bigint NOT NULL,
+    gender text,
     region text,
-    disablility text,
-    final_result text,
+    highest_education text,
+    imd_brand text,
+    age_band text,
     num_of_prev_attempts int,
     studied_credits int,
+    disablility text,
+    final_result text,
     CONSTRAINT student_key PRIMARY KEY (id_student),
     FOREIGN KEY (code_module, code_presentation) references courses(code_module, code_presentation)
     
@@ -48,8 +48,8 @@ CREATE table IF NOT EXISTS vle(
 );
 
 CREATE table IF NOT EXISTS studentAssessment(
-    id_student bigint NOT NULL,
     id_assessment int NOT NULL,
+    id_student bigint NOT NULL,
     date_submitted int,
     is_banked int,
     score  float,
@@ -58,10 +58,10 @@ CREATE table IF NOT EXISTS studentAssessment(
 );
 
 CREATE table IF NOT EXISTS studentVle(
-    id_site int NOT NULL,
-    id_student bigint NOT NULL,
     code_module text,
     code_presentation text,
+    id_student bigint NOT NULL,
+    id_site int NOT NULL,
     date int,
     sum_click int,
     FOREIGN KEY (code_module, code_presentation) references courses,
@@ -70,9 +70,9 @@ CREATE table IF NOT EXISTS studentVle(
 );
 
 CREATE table IF NOT EXISTS studentRegistration(
-    id_student bigint NOT NULL,
     code_module text,
     code_presentation text,
+    id_student bigint NOT NULL,
     date_registration int,
     date_unregistration int,
     FOREIGN KEY (code_module, code_presentation) references courses,
@@ -81,6 +81,19 @@ CREATE table IF NOT EXISTS studentRegistration(
 
 
 
+\COPY courses FROM '/Users/akhilesh/Desktop/col362_project/SAVLIP/courses.csv' DELIMITER ',' CSV HEADER;
+
+
+/* for assessments table, do similar for others*/
+CREATE TEMP TABLE assessments_tmp AS TABLE assessments LIMIT 0;
+ALTER TABLE assessments_tmp ALTER date TYPE text;
+\COPY assessments_tmp FROM '/Users/akhilesh/Desktop/col362_project/SAVLIP/assessments.csv' DELIMITER ',' CSV HEADER;
+INSERT INTO assessments
+SELECT code_module, code_presentation, id_assessment, assessment_type, NULLIF(date, '')::int, weight --type casting error is solved here
+FROM   assessments_tmp;
+
+/*
+
 \COPY courses FROM 'courses.csv' DELIMITER ',' CSV HEADER;
 \COPY assessments FROM 'assessments.csv' DELIMITER ',' CSV HEADER;
 \COPY studentInfo FROM 'studentInfo.csv' DELIMITER ',' CSV HEADER;
@@ -88,3 +101,5 @@ CREATE table IF NOT EXISTS studentRegistration(
 \COPY studentRegistration FROM 'studentRegistration.csv' DELIMITER ',' CSV HEADER;
 \COPY studentVle FROM 'studentVle.csv' DELIMITER ',' CSV HEADER;
 \COPY vle FROM 'vle.csv' DELIMITER ',' CSV HEADER;
+
+*/
