4a494ea711a9eaa28e6b16db3cf6749564e63144
aryan3514 <aryanyadav3514@gmail.com>
2022-02-09 11:56:51 +0530

    made many changes in create table

--- create_table.sql
+++ create_table.sql
@@ -1,4 +1,86 @@
-CREATE table IF NOT EXISTS hello_hii(
-    hello_id bigint NOT NULL,
-    CONSTRAINT hello_key PRIMARY KEY (hello_id)
+
+CREATE table IF NOT EXISTS courses(
+   code_module text,
+   code_presentation text,
+   module_presentation int,
+   PRIMARY KEY (code_module, code_presentation)
 );
+
+CREATE table IF NOT EXISTS assessments(
+    id_assessment int NOT NULL,
+    code_module text,
+    code_prensentation text,
+    assessment_type text,
+    date int,
+    weight float,
+    CONSTRAINT assessments_key PRIMARY KEY (id_assessment),
+    FOREIGN KEY (code_module) references courses(code_module),
+    FOREIGN KEY (code_presentation) references courses(code_presentation)
+);
+
+CREATE table IF NOT EXISTS studentInfo(
+    id_student bigint NOT NULL,
+    gender VARCHAR (3),
+    code_module text,
+    code_presentation text,
+    imd_brand text,
+    highest_education text,
+    age_band text,
+    region text,
+    disablility text,
+    final_result text,
+    num_of_prev_attempts int,
+    studied_credits int,
+    CONSTRAINT student_key PRIMARY KEY (id_student),
+    FOREIGN KEY (code_module) references courses(code_module),
+    FOREIGN KEY (code_presentation) references courses(code_presentation)
+);
+
+CREATE table IF NOT EXISTS vle(
+    id_site int NOT NULL,
+    code_module text,
+    code_presentation text,
+    activity_type text,
+    week_from int,
+    week_to int,
+    CONSTRAINT vle_key PRIMARY KEY (id_site),
+    FOREIGN KEY (code_module) references courses(code_module),
+    FOREIGN KEY (code_presentation) references courses(code_presentation)
+);
+
+CREATE table IF NOT EXISTS studentAssessment(
+    id_student bigint NOT NULL,
+    id_assessment int NOT NULL,
+    date_submitted int,
+    is_banked int,
+    score  float,
+    FOREIGN KEY (id_student) references studentInfo(id_student),
+    FOREIGN KEY (id_assessment) references assessments(id_assessment)  
+);
+
+CREATE table IF NOT EXISTS studentVle(
+    id_site int NOT NULL,
+    id_student bigint NOT NULL,
+    code_module text,
+    code_presentation text,
+    date int,
+    sum_click int,
+    FOREIGN KEY (code_module) references courses(code_module),
+    FOREIGN KEY (code_presentation) references courses(code_presentation),
+    FOREIGN KEY (id_site) references vle(id_site),
+    FOREIGN KEY (id_student) references studentInfo(id_student)
+);
+
+CREATE table IF NOT EXISTS studentRegistration(
+    id_student bigint NOT NULL,
+    code_module text,
+    code_presentation text,
+    date_registration int,
+    date_unregistration int,
+    FOREIGN KEY (code_module) references courses(code_module),
+    FOREIGN KEY (code_presentation) references courses(code_presentation),
+    FOREIGN KEY (id_student) references studentInfo(id_student)
+);
+
+
+
