79bb458df25db0f719e0f105cd3f35a0a9b8a356
aryan3514 <aryanyadav3514@gmail.com>
2022-02-21 03:34:00 +0530

    css related UI almost finished

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
 
+@app.route('/')
+def main():
+    return render_template('entry.html')
 
 @app.route('/studentpage')
 def studentpage():
